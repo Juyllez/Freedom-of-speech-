c32b2feaa2f780329d93aa8fde62ec032f0fca32
Juyllez <julia.tischler@hfg-gmuend.de>
2025-04-03 13:56:29 +0200

    1

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let table; 
+let points = [];
+
+function preload() {
+    table = loadTable('freedom_with_continent.csv', 'csv', 'header');
+}
+
+function setup() {
+    createCanvas(1280, 720); 
+    colorMode(HSB, 360, 100, 100);
+    console.log("number of continents: ");
+
+}
+function draw() {
+    background (51);
+
+}
+
